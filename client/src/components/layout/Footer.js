be9236de75b58a222cbaf6b45c51a3a5f5def872
Richard Mena <richardmf9425@Richards-MacBook-Pro.local>
2019-11-04 12:41:02 -0500

    add footer elements and some styling(not finished)

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer() {
 	return (
 		<div className="footer">
-			<h1 className="x-large">Footer</h1>
+			<div className="subscribe">Subscribe to Newsletter</div>
+			<div className="info-copyright">
+				<h3>Wellness Network</h3>
+				<h3>Copyright 2019 &copy; All Rights Reserved.</h3>
+			</div>
+			<div className="social-media">
+				<Link> <i className="fab fa-facebook"></i></Link>
+				<Link> <i className="fab fa-youtube"></i></Link>
+				<Link> <i className="fab fa-instagram"></i></Link>
+			</div>
+
 		</div>
 	);
 }
