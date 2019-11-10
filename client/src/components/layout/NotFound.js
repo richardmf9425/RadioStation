3faa537b929957618688e3e4538582e6806178c1
Richard Mena <richardmf9425@yahoo.com>
2019-11-09 22:58:03 -0500

    add not found page

---
+++ client/src/components/layout/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NotFound() {
+	return (
+		<div className="not-found-section">
+			<i id="err-icon" class="fa fa-exclamation-circle" />
+			<h1>404 Not Found!</h1>
+			<p>Oops... the page you are looking for isn't here!</p>
+			<Link to="/" className="btn">
+				Go Back
+			</Link>
+		</div>
+	);
+}
+
+export default NotFound;
