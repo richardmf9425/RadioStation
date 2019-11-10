6640357a32946327f8053e551f43a304382d1441
Richard Mena <richardmf9425@yahoo.com>
2019-11-09 22:58:49 -0500

    styled 404 not found component

--- client/src/components/layout/NotFound.js
+++ client/src/components/layout/NotFound.js
@@ -7,7 +7,7 @@ function NotFound() {
 			<i id="err-icon" class="fa fa-exclamation-circle" />
 			<h1>404 Not Found!</h1>
 			<p>Oops... the page you are looking for isn't here!</p>
-			<Link to="/" className="btn">
+			<Link to="/" className="btn btn-primary">
 				Go Back
 			</Link>
 		</div>
