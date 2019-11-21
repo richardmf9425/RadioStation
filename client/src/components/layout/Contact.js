5c6c6fa182526e0774acd2f7ed1c74a1890235c1
Richard Mena <richardmf9425@yahoo.com>
2019-11-20 22:53:27 -0500

    add enformed.io token

--- client/src/components/layout/Contact.js
+++ client/src/components/layout/Contact.js
@@ -2,23 +2,32 @@ import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
 toast.configure();
 
 function Contact() {
 	const [ fireRedirect, setFireRedirect ] = useState(false);
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		setFireRedirect(true);
-		toast.success('Thank you for contacting us! We will get back to you as soon as possible', {
-			bodyClassName: 'toast-background',
-			position: 'top-left',
-			autoClose: 4000,
-			hideProgressBar: true,
-			closeOnClick: true,
-			pauseOnHover: false,
-			draggable: true
-		});
-	};
+	const [ formInfo, setFormInfo ] = useState({
+		name: '',
+		email: '',
+		message: ''
+	});
+	const { name, email, message } = formInfo;
+	const handleChange = (e) => setFormInfo({ ...formInfo, [e.target.name]: e.target.value });
+
+	// const handleSubmit = (e) => {
+	// 	e.preventDefault();
+	// 	setFireRedirect(true);
+	// 	toast.success('Thank you for contacting us! We will get back to you as soon as possible', {
+	// 		bodyClassName: 'toast-background',
+	// 		position: 'top-left',
+	// 		autoClose: 4000,
+	// 		hideProgressBar: true,
+	// 		closeOnClick: true,
+	// 		pauseOnHover: false,
+	// 		draggable: true
+	// 	});
+	//};
 	return (
 		<div className="contact-section">
 			<div class="contact">
@@ -26,13 +35,18 @@ function Contact() {
 					<h3>Get in Touch with us</h3>
 					<i class="fa fa-comment fa-lg" />
 				</div>
-				<form onSubmit={(e) => handleSubmit(e)}>
+				<form action="https://www.enformed.io/awrr3hsj" method="POST">
 					<label>Name</label>
-					<input type="text" name="name" />
+					<input type="text" name="name" value={name} onChange={(e) => handleChange(e)} />
 					<label>Email</label>
-					<input type="email" name="email" />
+					<input type="email" name="email" value={email} onChange={(e) => handleChange(e)} />
 					<label>Message</label>
-					<textarea placeholder="Enter Your Message" name="message" />
+					<textarea
+						placeholder="Enter Your Message"
+						name="message"
+						value={message}
+						onChange={(e) => handleChange(e)}
+					/>
 
 					<button type="submit">Send Message</button>
 				</form>
