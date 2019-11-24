49c5155a8733c5629e80d4202fc7cc74e8bb7f0b
Andrew So <so.andrew0@gmail.com>
2019-11-23 22:45:44 -0500

    Basic AWS S3 upload implementation

--- client/src/components/layout/FileUploader.js
+++ client/src/components/layout/FileUploader.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import ReactS3 from 'react-s3';
+
+const config = {
+	bucketName: 'atthewellnessnetwork',
+	dirName: 'uploads',
+	region: 'us-east-1',
+	accessKeyId: 'AKIAS264P6ATCSGV3RIW',
+	secretAccessKey: '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',
+}
 
 function FileUploader() {
+	const uploadFile = (e) => {
+		console.log(e.target.files[0]);
+		ReactS3.uploadFile(e.target.files[0], config)
+		.then((data) => {
+			console.log(data);
+		})
+		.catch((err) => {
+			console.error(err);
+			alert(err);
+		})
+	}
+
 	return (
 		<div className="file-uploader">
-			<iframe
-				src="https://script.google.com/macros/s/AKfycbwEtaOInyqzEJnNxj6BBPhPKZPFRzXPWbKXauQ1CxYBeTruBKX8/exec"
-				height="99%"
-				width="99%"
-				position="absolute"
-				border="none"
-			/>
+			<input 
+			type="file"
+			onChange={uploadFile}>
+			</input>
 		</div>
 	);
 }
